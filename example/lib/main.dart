e88443dae7d8af00f7428fbc9016b2f1b9d1f083
Feelfree <rj23@protonmail.com>
2018-12-22 00:19:17 +0100

    feat: Color picker in example

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -3,7 +3,11 @@ import 'package:athos/athos.dart';
 import 'package:shared_pref_annotation/shared_pref_annotation.dart';
 import 'package:rxdart/rxdart.dart';
 import 'dart:async';
+import 'package:flutter_colorpicker/flutter_colorpicker.dart';
 part 'main.g.dart';
+
+Color pickerColor = Color(0xff443a49);
+Color currentColor = Color(0xff443a49);
 
 void main() => runApp(MyApp());
 
@@ -20,51 +24,85 @@ class MyApp extends StatelessWidget {
   }
 }
 
-
-
 @ReactivePreferencesHolder()
 abstract class SharedSettings implements SharedSettingsPreferences {
   int counter;
+  int appbarColor;
 
   factory SharedSettings(PreferenceAdapter adapter) = _$SharedSettings;
 }
-
 
 class MyHomePage extends StatelessWidget {
   final SharedSettings settings = SharedSettings(SharedPreferencesAdapter());
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Generated settings demo"),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+    return StreamBuilder<int>(
+      stream: settings.appbarColorStream,
+      builder: (BuildContext context, AsyncSnapshot appbarColorSnapshot) {
+        return Container(
+          child: Scaffold(
+            appBar: AppBar(
+              backgroundColor: Color(appbarColorSnapshot.data ?? 0),
+              title: Text("Generated settings demo"),
+              actions: <Widget>[
+                new IconButton(
+                    icon: new Icon(Icons.colorize),
+                    tooltip: 'Change appbar color',
+                    onPressed: () {
+                      showDialog(
+                          context: context,
+                          builder: (context) => AlertDialog(
+                                  title: const Text('Pick a color!'),
+                                  content: SingleChildScrollView(
+                                    child: ColorPicker(
+                                      pickerColor: pickerColor,
+                                      onColorChanged: (color) =>
+                                          settings.appbarColor = color.value,
+                                      enableLabel: true,
+                                      pickerAreaHeightPercent: 0.8,
+                                    ),
+                                  ),
+                                  actions: <Widget>[
+                                    FlatButton(
+                                      child: Text('Got it'),
+                                      onPressed: () {
+                                        Navigator.of(context).pop();
+                                      },
+                                    ),
+                                  ]));
+                    }),
+              ],
             ),
-            StreamBuilder<int>(
-              stream: settings.counterStream,
-              builder: (BuildContext context, AsyncSnapshot snapshot) {
-                return Container(
-                  child: Text(
-                    '${snapshot.data ?? 0}',
-                    style: Theme.of(context).textTheme.display1,
+            body: Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Text(
+                    'You have pushed the button this many times:',
                   ),
-                );
-              },
+                  StreamBuilder<int>(
+                    stream: settings.counterStream,
+                    builder: (BuildContext context, AsyncSnapshot snapshot) {
+                      return Container(
+                        child: Text(
+                          '${snapshot.data ?? 0}',
+                          style: Theme.of(context).textTheme.display1,
+                        ),
+                      );
+                    },
+                  ),
+                ],
+              ),
             ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () => settings.counter = (settings.counter ?? 0) + 1,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ),
+            floatingActionButton: FloatingActionButton(
+              onPressed: () => settings.counter = (settings.counter ?? 0) + 1,
+              tooltip: 'Increment',
+              child: Icon(Icons.add),
+            ),
+          ),
+        );
+      },
     );
   }
 }
--- example/lib/main.g.dart
+++ example/lib/main.g.dart
@@ -14,25 +14,43 @@ class _$SharedSettings implements SharedSettings {
             _counterSubject.add(value);
           }))
         .sink;
+    appbarColorSink = (StreamController<int>.broadcast()
+          ..stream.listen((value) async {
+            await adapter.setInt("appbarColor", value);
+            _appbarColorSubject.add(value);
+          }))
+        .sink;
     _adapter.getInt("counter").then(counterSink.add);
+    _adapter.getInt("appbarColor").then(appbarColorSink.add);
   }
 
   final PreferenceAdapter _adapter;
 
   final BehaviorSubject<int> _counterSubject = BehaviorSubject<int>();
 
+  final BehaviorSubject<int> _appbarColorSubject = BehaviorSubject<int>();
+
   Sink<int> counterSink;
 
+  Sink<int> appbarColorSink;
+
   Stream<int> get counterStream => _counterSubject.asBroadcastStream();
+  Stream<int> get appbarColorStream => _appbarColorSubject.asBroadcastStream();
   int get counter => _counterSubject.value;
+  int get appbarColor => _appbarColorSubject.value;
   set counter(int value) => counterSink.add(value);
+  set appbarColor(int value) => appbarColorSink.add(value);
   void dispose() {
     counterSink.close();
+    appbarColorSink.close();
   }
 }
 
 abstract class SharedSettingsPreferences {
   Sink<int> counterSink;
 
+  Sink<int> appbarColorSink;
+
   Stream<int> get counterStream;
+  Stream<int> get appbarColorStream;
 }
